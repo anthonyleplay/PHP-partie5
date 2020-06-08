0a589c8312ee8fa46843db01ce3bad64fa996a7c
Anthony Le Play <anthony.leplay@gmail.com>
2020-06-08 11:12:10 +0200

    creation des index

---
+++ exercice3/index.php
@@ -0,0 +1,33 @@
+<?php
+
+function myfunction() {
+    return true;
+    } ;
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo1</title>
+</head>
+
+<body>
+    <h1>Exercice 1</h1>
+
+    <p>Faire une fonction qui retourne true.</p>
+   
+
+    <p>===================================</p>
+
+    <p><?= myfunction() ?> </p>
+
+
+</body>
+
+</html>
